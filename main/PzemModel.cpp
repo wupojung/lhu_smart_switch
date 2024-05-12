081168e89c0df068b616eed836220a5c16870ce2
Po-Lung Wu <wupojung@gmail.com>
2024-05-13 03:12:34 +0800

    調整宣告

--- main/PzemModel.cpp
+++ main/PzemModel.cpp
@@ -1,29 +1,31 @@
-
+//
+// Created by wupolung on 2024/5/13.
+//
 #include "PzemModel.h"
 #include <ArduinoJson.h>
 
 
 PzemModel::PzemModel() {
 
-     Voltage=0.0f;
-     Current=0.0f;
-     Power=0.0f;
-     Energy=0.0f;
-     Frequency=0.0f;
-     PF=0.0f;
-
+    Voltage = 0.0f;
+    Current = 0.0f;
+    Power = 0.0f;
+    Energy = 0.0f;
+    Frequency = 0.0f;
+    PF = 0.0f;
 }
+
 String PzemModel::ToJson() {
-  StaticJsonDocument<200> doc;
+    StaticJsonDocument<200> doc;
 
-  String output;
-  doc["Voltage"]=Voltage;
-  doc["Current"]=Current;
-  doc["Power"]=Power;
-  doc["Energy"]=Energy;
-  doc["Frequency"]=Frequency;
-  doc["PF"]=PF;
+    String output;
+    doc["Voltage"] = Voltage;
+    doc["Current"] = Current;
+    doc["Power"] = Power;
+    doc["Energy"] = Energy;
+    doc["Frequency"] = Frequency;
+    doc["PF"] = PF;
 
-  serializeJson(doc, output);
-  return output;
+    serializeJson(doc, output);
+    return output;
 }
--- main/PzemModel.h
+++ main/PzemModel.h
@@ -1,15 +1,21 @@
+//
+// Created by wupolung on 2024/5/13.
+//
 #define PZEM_MODEL_H
 #define PZEM_MODEL_H
+
 #include <ArduinoJson.h>
 
 class PzemModel {
-  public:
+public:
     PzemModel();
+
     float Voltage;
     float Current;
     float Power;
     float Energy;
     float Frequency;
     float PF;
+
     String ToJson();
 };
